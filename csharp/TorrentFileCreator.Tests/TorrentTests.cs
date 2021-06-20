5f5752fe99e0c2b14b5bdfacb207683586dc26fe
Mikael Frosthage <mikael.frosthage@gmail.com>
2021-06-20 16:37:00 -0400

    Pr cleanup

--- csharp/TorrentFileCreator.Tests/TorrentTests.cs
+++ csharp/TorrentFileCreator.Tests/TorrentTests.cs
@@ -27,31 +27,5 @@ namespace TorrentFileCreator.Tests
             
             torrentFileHashValue.ShouldBe("lKT0ZBMYgv3OzArI6Utytw==");
         }
-        
-        [Fact]
-        public async Task Bepurt()
-        {
-            var md5 = MD5.Create();
-            var fileStream = new MemoryStream();
-            var torrent = new Torrent(new StreamWriter(fileStream));
-            await torrent.CreateAsync(@"C:\Program Files (x86)\Steam");
-
-            var torrentFileHashValue = Convert.ToBase64String(md5.ComputeHash(fileStream.ToArray()));
-            
-            torrentFileHashValue.ShouldBe("lKT0ZBMYgv3OzArI6Utytw==");
-        }
-        
-        [Fact]
-        public async Task asfasdfasdfas()
-        {
-            var md5 = MD5.Create();
-            var fileStream = File.OpenWrite(@"c:\slask\unsorted.torrent");
-            var torrent = new Torrent(new StreamWriter(fileStream));
-            await torrent.CreateAsync(@"C:\Program Files (x86)\Steam");
-
-            //var torrentFileHashValue = Convert.ToBase64String(md5.ComputeHash(fileStream.ToArray()));
-            
- //1           torrentFileHashValue.ShouldBe("lKT0ZBMYgv3OzArI6Utytw==");
-        }
     }
 }
--- go/go.mod
+++
@@ -1,1 +0,0 @@
-module github.com/Frosthage/torrent-file-creator
